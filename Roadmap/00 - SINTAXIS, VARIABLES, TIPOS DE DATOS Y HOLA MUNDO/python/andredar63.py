b72c7e85e9ffef42e51eeb64a0fcb1b7f7028787
ANDREDAR63 <andred.ar63@gmail.com>
2024-11-30 12:25:31 -0500

    Create andredar63.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/andredar63.py
@@ -0,0 +1,31 @@
+# https://www.python.org/
+# para comentario en  una sola linea de codigo
+"""
+para dejar comentarios
+en varias lineas de codigo
+"""
+'''
+tambien para dejar comentarios
+en varias lineas de codigo
+'''
+#variables
+#como crear una variable en python
+my_variable = "mi variable"
+#redefiniendo variable
+my_variable = " mi nueva variable
+
+#python no tiene constantes pero para los programadores se representa en MAYUSCULAS PARA IDENTIFICAR QUE NO SE DEBE CAMBIAR
+
+MY_CONSTANT = "Mi constante"
+my_int = 1
+my_float = 1.5
+my_bool = true
+my_bool = false
+my_string = "mi cadena de texto"
+
+print= (Hola, phyton)
+print (type (my_int)
+print (type (my_float)
+print (type (my_bool)
+print (type (my_string)
+
